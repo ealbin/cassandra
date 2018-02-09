ddcf6bcedca262337bae2ef07779441d5ee58df6
Eric Albin <Eric.K.Albin@gmail.com>
2018-02-09 00:02:53 +0000

    fixed cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -10,47 +10,46 @@ HOST_DATA="/data/daq.crayfis.io/raw"
 HOST_SRC="$PWD/src"
 
 PS3="Select Command: "
-commands=("Set Variables" "Boot up ${CASSANDRA_IMAGE}" "Build and Boot ${HOST_IMAGE}" "Log into ${CLUSTER_NAME}" "Quit")
+commands=("Boot up ${CASSANDRA_IMAGE}" "Build and Boot ${HOST_IMAGE}" "Log into ${CLUSTER_NAME}" "Clean docker images")
 select opt in "${commands[@]}"
 do
     case $opt in
-        "Set Paths")
-            echo "haha you can't yet, edit the damn file"
-            ;;
         "Boot up ${CASSANDRA_IMAGE}")
-	    cmd="docker run --name ${CLUSTER_NAME} -v ${HOST_CASSANDRA_DIR}:/var/lib/cassandra -d ${CASSANDRA_IMAGE}"
-	    echo
-	    echo $cmd
-	    eval $cmd
+            check=`docker ps | egrep -c "$CLUSTER_NAME"`
+            if [ $check -gt 0 ]; then echo "instance of $CLUSTER_NAME already running..."; break; fi
+	        cmd="docker run --name ${CLUSTER_NAME} -v ${HOST_CASSANDRA_DIR}:/var/lib/cassandra -d ${CASSANDRA_IMAGE}"
+	        echo
+	        echo $cmd
+	        eval $cmd
             echo
-	    break
+	        break
             ;;
         "Build and Boot ${HOST_IMAGE}")
-	    cmd="docker build -t ${HOST_IMAGE} ."
-	    echo
-	    echo $cmd
-	    eval $cmd
-	    exit_code=$?
-	    echo
+	        cmd="docker build -t ${HOST_IMAGE} ."
+	        echo
+	        echo $cmd
+	        eval $cmd
+	        exit_code=$?
+	        echo
             if [[ $exit_code != 0 ]]; then break; fi
-	    cmd="docker run --rm --name ${HOST_NAME} -v ${HOST_DATA}:/data/daq.crayfis.io/raw -v ${HOST_SRC}:/home/${HOST_NAME}/src --link ${CLUSTER_NAME}:cassandra -it ${HOST_IMAGE}"
-	    echo $cmd
-	    eval $cmd
-	    echo
-	    break
+	        cmd="docker run --rm --name ${HOST_NAME} -v ${HOST_DATA}:/data/daq.crayfis.io/raw -v ${HOST_SRC}:/home/${HOST_NAME}/src --link ${CLUSTER_NAME}:cassandra -it ${HOST_IMAGE}"
+	        echo $cmd
+	        eval $cmd
+	        echo
+	        break
             ;;
-	"Log into ${CLUSTER_NAME}")
-	    cmd="docker run --it --link ${CLUSTER_NAME}:cassandra --rm cassandra cqlsh cassandra"
-	    echo 
-	    echo $cmd
-	    eval $cmd
-	    echo
-	    break
-	    ;;
-        "Quit")
-	    echo "done."
-	    break
-            ;;
+        "Log into ${CLUSTER_NAME}")
+	        cmd="docker run -it --link ${CLUSTER_NAME}:cassandra --rm cassandra cqlsh cassandra"
+	        echo 
+	        echo $cmd
+	        eval $cmd
+	        echo
+	        break
+	        ;;
+	    "Clean docker images")
+    	    for id in `docker images | egrep "^<none>" | awk '{print $3}'`; do docker rmi $id; done
+    	    break
+    	    ;;
         *) echo invalid option;;
     esac
 done
