6c7b490634ea15d05fd01772f8e6491604cb5f49
Eric Albin <Eric.K.Albin@gmail.com>
2018-02-10 00:11:46 +0000

    update cmd.sh

--- bin/cmd.sh
+++ bin/cmd.sh
@@ -12,24 +12,25 @@ HOST_SRC="$PWD/src"
 update() {
     check=`docker ps | egrep -c "${HOST_NAME}"`
     if [ $check -gt 0 ]; then docker kill ${HOST_NAME}; docker rm ${HOST_NAME}; fi
-	cmd="docker build -t ${HOST_IMAGE} ."
-	echo
-	echo $cmd
-	eval $cmd
-	exit_code=$?
-	echo
+    cmd="docker build -t ${HOST_IMAGE} ."
+    echo
+    echo $cmd
+    eval $cmd
+    exit_code=$?
+    echo
     if [[ $exit_code != 0 ]]; then break; fi
     data_map="${HOST_DATA}:/data/daq.crayfis.io/raw"
     src_map="${HOST_SRC}:/home/${HOST_NAME}/src"
     ingested_map="${HOST_SRC}/ingested"
-	cmd="docker run --rm --name ${HOST_NAME} -v ${data_map} -v ${src_map} -v ${ingested_map} --link ${CLUSTER_NAME}:cassandra -dt ${HOST_IMAGE}"
-	echo $cmd
+    cmd="docker run --rm --name ${HOST_NAME} -v ${data_map} -v ${src_map} -v ${ingested_map} --link ${CLUSTER_NAME}:cassandra -dt ${HOST_IMAGE}"
+    echo $cmd
     eval $cmd
-	echo
-	cmd="docker exec ${HOST_NAME} python /home/${HOST_NAME}/src/update.py"
-	echo $cmd
-	eval $cmd
-	echo
+    echo
+    cmd="docker exec ${HOST_NAME} python /home/${HOST_NAME}/src/update.py"
+    echo $cmd
+    eval $cmd
+    echo
+    docker kill ${HOST_NAME}
 }
 
 if [ $# -eq 1 ]; then
